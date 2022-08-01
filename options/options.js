a88d16d2ff264d533dfa5b950ac3336283c5dc89
Crowdbotics <team@crowdbotics.com>
2022-08-01 19:31:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "helping_hands_tutor_36543",
+  url: "https://helping_hands_tutor_36543.botics.co",
+  api: "https://helping_hands_tutor_36543.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
